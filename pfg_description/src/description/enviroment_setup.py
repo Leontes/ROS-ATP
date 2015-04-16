6ede6dc294c356572fe952ef3c408a8566a542e2
Leontes <theshaljhas@gmail.com>
2015-04-16 16:12:50 +0200

    Minor bug fix solved with RviZ

--- pfg_description/src/description/enviroment_setup.py
+++ pfg_description/src/description/enviroment_setup.py
@@ -1,48 +1,3 @@
-"""
-
-import rospy
-
-from visualization_msgs.msg import Marker
-
-from pfg_tasks import global_vars
-
-def init_environment():
-    # Initialize the marker points list.
-    waypoint_list = Marker()
-    waypoint_list.ns = 'Waypoints'
-    waypoint_list.id = 0
-    waypoint_list.type = Marker.CUBE_LIST
-    waypoint_list.action = Marker.ADD
-    waypoint_list.lifetime = rospy.Duration(0)
-    waypoint_list.scale.x = 0.2
-    waypoint_list.scale.y = 0.2
-    waypoint_list.scale.z = 0.05
-    waypoint_list.color.r = 1.0
-    waypoint_list.color.g = 0.7
-    waypoint_list.color.b = 1.0
-    waypoint_list.color.a = 1.0
-
-    waypoint_list.header.frame_id = 'odom'
-    waypoint_list.header.stamp = rospy.Time.now()
-    waypoint_list.points = list()
-
-    coords = global_vars.black_board.getAllCoords()
-    # Set a visualization marker at each waypoint        
-    for pt in coords:     
-        waypoint_list.points.append(coords[pt].position)
-
-    # Define a marker publisher.
-    marker_pub = rospy.Publisher('waypoints', Marker, queue_size = 10)
-
-    # Publish the waypoint markers
-    marker_pub.publish(waypoint_list)
-
-
-
-"""
-
-
-
 import rospy
 
 from visualization_msgs.msg import Marker
@@ -53,7 +8,7 @@ def init_environment():
     # Initialize the marker points list.
     waypoint_list = Marker()
 
-    waypoint_list.ns = 'Waypoints'
+    waypoint_list.ns = 'WaypointsNS'
 
     waypoint_list.id = 0
 
@@ -80,12 +35,15 @@ def init_environment():
     i = 1
 
     # Define a marker publisher.
-    marker_pub = rospy.Publisher('waypoint_list', Marker, queue_size = 10) 
+    marker_pub = rospy.Publisher('waypointsMarkers', Marker, queue_size = 10) 
+
+    marker_pub.publish(waypoint_list)
+    rospy.sleep(1)
     
     for pt in coords:
         waypointText = Marker()
 
-        waypointText.ns = 'Waypoints'
+        waypointText.ns = 'WaypointsNS'
 
         waypointText.id = i
 
@@ -95,7 +53,7 @@ def init_environment():
 
         waypointText.lifetime = rospy.Duration(0)
         
-        waypointText.scale.z = 2.0
+        waypointText.scale.z = 0.25
 
         waypointText.color.r = 0.5
         waypointText.color.g = 0.7
@@ -104,6 +62,11 @@ def init_environment():
         
         waypointText.text = pt
         waypointText.pose = coords[pt]
+
+        waypointText.header.frame_id = 'odom'
+        waypointText.header.stamp = rospy.Time.now()
+
+        waypointText.pose.position.z += 0.02
 
         marker_pub.publish(waypointText)
         i = i+1
@@ -115,4 +78,3 @@ def init_environment():
 
     # Publish the waypoint markers
     marker_pub.publish(waypoint_list)
-
