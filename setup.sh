e676abdc92b67f8a7351a15b1bb7b14d3c90e379
Leontes <theshaljas@gmail.com>
2015-07-05 18:17:07 +0200

    Fixed minor bug in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-chmod 777 src/PFG/atp_utils/nodes/*.py
-chmod 777 src/PFG/atp_tasks/nodes/*.py
-chmod 777 src/PFG/atp_utils/cfg/BatterySimulator.cfg
+chmod 777 src/ROS-ATP/atp_utils/nodes/*.py
+chmod 777 src/ROS-ATP/tasks_exec/nodes/*.py
+chmod 777 src/ROS-ATP/atp_utils/cfg/BatterySimulator.cfg
 cd src/
 git clone https://github.com/vanadiumlabs/arbotix_ros.git
 cd ..
