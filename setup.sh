eddd69201d7f4fb699b1ff81b79145103ad4e5f2
Jose Angel Segura Muros <josesegmur@gmail.com>
2016-03-14 10:53:24 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 chmod 777 src/ROS-ATP/atp_utils/nodes/*.py
 chmod 777 src/ROS-ATP/tasks_exec/nodes/*.py
 chmod 777 src/ROS-ATP/atp_utils/cfg/BatterySimulator.cfg
-cd src/
-git clone https://github.com/vanadiumlabs/arbotix_ros.git
-cd ..
-apt-get install ros-indigo-rviz
+
+apt-get install -y ros-indigo-arbotix ros-indigo-rviz
+
 catkin_make
 source devel/setup.bash
