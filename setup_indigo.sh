a126cad51764bf6ead5cdb852448f6e45cf024ff
Leontes <theshaljas@gmail.com>
2016-04-01 15:16:34 +0200

    Fixed some typo in the scripts

--- setup_indigo.sh
+++ setup_indigo.sh
@@ -4,8 +4,8 @@ chmod 777 src/ROS-ATP/atp_utils/nodes/*.py
 chmod 777 src/ROS-ATP/tasks_exec/nodes/*.py
 chmod 777 src/ROS-ATP/atp_utils/cfg/BatterySimulator.cfg
 
-apt_get update -y
-apt-get install -y ros-indigo-arbotix ros-indigo-rviz
+apt-get update -y --force-yes
+apt-get install -y --force-yes ros-indigo-arbotix ros-indigo-rviz
 apt-get upgrade -y --force-yes
 
 source /opt/ros/indigo/setup.bash
