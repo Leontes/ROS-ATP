314b2cff544c697d8a9333461fa3ee6ad905a210
Jose Angel Segura Muros <josesegmur@gmail.com>
2016-07-07 10:53:59 +0200

    Update setup_indigo.sh

--- setup_indigo.sh
+++ setup_indigo.sh
@@ -5,7 +5,7 @@ chmod 777 src/ROS-ATP/tasks_exec/nodes/*.py
 chmod 777 src/ROS-ATP/atp_utils/cfg/BatterySimulator.cfg
 
 apt-get update -y --force-yes
-apt-get install -y --force-yes ros-indigo-arbotix ros-indigo-rviz
+apt-get install -y --force-yes ros-indigo-arbotix ros-indigo-rviz ros-indigo-fake-localization ros-indigo-map-server
 apt-get upgrade -y --force-yes
 
 source /opt/ros/indigo/setup.bash
